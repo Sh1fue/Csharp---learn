9646639ab19b6799a7ebe7f5c0b472856938737c
Sh1fue <chirik270@gmail.com>
2025-04-01 21:47:37 +0300

    Добавьте файлы проекта.

---
+++ 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prikol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Введите число вклада");
+            int Vklad = Convert.ToInt32(Console.ReadLine());
+
+            if (Vklad < 100)
+            {
+                Console.WriteLine($" Ваш вклад {Vklad} + С бонусами {Vklad + Vklad * 0.05}");
+            }
+            else if (Vklad >= 100 && Vklad <= 200)
+            {
+                Console.WriteLine($"Ваш вклад {Vklad} + С бонусами {Vklad + Vklad * 0.07}");
+            }
+            else if (Vklad > 200)
+            {
+                Console.WriteLine($"Ваш вклад {Vklad} + С бонусами {Vklad + Vklad * 0.1}");
+            }
+
+        }
+    }
+}
